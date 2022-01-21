93ebbc2c125815b8d906d73d5fc2731c67b41233
Ali Raheem <ali-windows@shoryuken.me>
2022-01-21 17:20:00 +0000

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use std::env;
 use std::process::exit;
 
 fn print_usage(name: &str) {
-    println!("Usage:
-    \t{} [u|p] iso
+    println!(
+        "Usage:
+    ./{} [u|p] FILE
     
     p - Apply ESR patch to ISO file.
-    u - Remove ESR patch to ISO file.
-    iso - A path to an ISO file.", name);
+    u - Remove ESR patch from ISO file.
+    FILE - A path to an ISO or BIN file.",
+        name
+    );
     exit(1);
 }
 
@@ -19,7 +22,7 @@ fn main() {
     match &args[1][..] {
         "p" => esrtool::patch(&args[2]),
         "u" => esrtool::unpatch(&args[2]),
-        _ => print_usage(&args[0])
+        _ => print_usage(&args[0]),
     }
     println!("OK!");
 }
